d72410ec372d86a3b6bd750367e1723fd3c2c8f3
Zhihao Zhang <zhangzhihao6@outlook.com>
2025-12-02 21:42:49 +0800

    y

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburgerMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-});
